e2cad5c58046d388ac087cf46dd30e12bd5989e9
b06fae0f <166984764+b06fae0f@users.noreply.github.com>
2025-12-18 15:39:07 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,14 @@
 @echo off
 
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe /out:Program.exe *.cs
+set CSCDIR=%WINDIR%\Microsoft.NET\
+set CSCVER=v4.0.30319
+
+if "%PROCESSOR_ARCHITECTURE%" == "x86" (
+    set CSCDIR=%CSCDIR%Framework\
+) else (
+    set CSCDIR=%CSCDIR%Framework64\
+)
+
+set CSCDIR=%CSCDIR%%CSCVER%\
+
+%CSCDIR%csc.exe /out:Program.exe *.cs
